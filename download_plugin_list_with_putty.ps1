efd94ebf2d00d2d4358a52d389541460b5c1f9ed
williamsh.huang <tisamwilliam@gmail.com>
2023-02-04 09:53:13 +0800

    feat: fix putty char escape

--- download_plugin_list_with_putty.ps1
+++ download_plugin_list_with_putty.ps1
@@ -11,20 +11,22 @@ $bastion_cred = Get-Credential -Message "請輸入Bastion主機 登入帳號/密
 $openshift_cred = Get-Credential -Message "請輸入Openshift 使用者帳號/密碼(${ocp_api_url})"
 $bastion_remote_control = "plink -no-antispoof -l $($bastion_cred.UserName) -pw $($bastion_cred.GetNetworkCredential().Password) $($bastion_ip_address)"
 
-# 如果Bastion主機的Openshift使用者不是$openshift_cred中輸入的, 會透過plink做oc login
-if (Invoke-Expression "$bastion_remote_control 'oc whoami 2>/dev/null' | findstr $openshift_cred.UserName" ){
-    Write-Output "Openshift API Server: ${ocp_api_url}"
-    Write-Output "Openshift Username: $($openshift_cred.UserName)"
-    Invoke-Expression "$bastion_remote_control 'oc login --username=$($openshift_cred.UserName) --password=$($openshift_cred.GetNetworkCredential().Password) --server=${ocp_api_url}'"
-}
+Write-Output "`n登入Openshift"
+Write-Output "Openshift API Server: ${ocp_api_url}"
+Write-Output "Openshift Username: $($openshift_cred.UserName)"
+Invoke-Expression "$bastion_remote_control oc login --username=$($openshift_cred.UserName) --password=$($openshift_cred.GetNetworkCredential().Password) --server=${ocp_api_url}"
 
-# 與Jenkins API連線取得套件清單
-Invoke-Expression "$bastion_remote_control 'curl -s --header 'Authorization: Bearer $(oc whoami -t)' -k https://$($jenkins_url)/pluginManager/api/xml\?depth\=1'" > jenkins_plugin_list.xml
+Write-Output "`n透過Jenkins API取得套件清單"
+Invoke-Expression "$bastion_remote_control 'curl --header \`"Authorization: Bearer `$(oc whoami -t)\`" -k https://$($jenkins_url)/pluginManager/api/xml\?depth\=1'" > jenkins_plugin_list.xml
 
-# 傳輸套件清單與執行Python腳本下載套件
+Write-Output "`n傳送套件清單到Mac主機"
 $mac_cred = Get-Credential -Message "請輸入Mac主機 登入帳號/密碼"
 $mac_remote_control = "plink -no-antispoof -pw $($mac_cred.GetNetworkCredential().Password) -l $($mac_cred.UserName) $($mac_ip_address)"
 
 pscp -l $mac_cred.UserName -pw $mac_cred.GetNetworkCredential().Password  jenkins_plugin_list.xml ${mac_ip_address}:"/home/$($mac_cred.UserName)/"
+
+Write-Output "`n從Git更新Python腳本"
 Invoke-Expression "$mac_remote_control 'cd /home/$($mac_cred.UserName)/jenkins-plugin-download; git pull'"
+
+Write-Output "`n執行Python腳本下載套件"
 Invoke-Expression "$mac_remote_control 'python3 /home/$($mac_cred.UserName)/jenkins-plugin-download/jenkins_download_plugin.py ${jenkins_version}'"
